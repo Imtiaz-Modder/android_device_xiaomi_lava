78483dd69d0f6bad2fa2a810f8c55e1ad934845f
Imtiaz Modder <67188507+Imtiaz-Modder@users.noreply.github.com>
2022-06-22 23:09:55 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/voltage_lava.mk
+   $(LOCAL_DIR)/lineage_lava.mk
 
 COMMON_LUNCH_CHOICES := \
-    voltage_lava-user \
-    voltage_lava-userdebug \
-    voltage_lava-eng
+    lineage_lava-user \
+    lineage_lava-userdebug \
+    lineage_lava-eng
